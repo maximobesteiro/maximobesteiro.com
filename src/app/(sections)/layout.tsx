b36bf0f447aaf8a6bd1eda60737d072d178cfbce
Máximo Besteiro <machibest@gmail.com>
2024-05-04 20:50:55 +0200

    Added some background color complexity

--- src/app/(sections)/layout.tsx
+++ src/app/(sections)/layout.tsx
@@ -5,7 +5,7 @@ import PageHeader from '@/components/pageHeader';
 const SectionLayout = ({ children }: { children: React.ReactNode }) => (
   <>
     <PageHeader />
-    <main className="container mx-auto space-y-10 px-6 pb-16 pt-20 md:space-y-14 md:pt-32 lg:px-8 2xl:max-w-7xl">
+    <main className="container mx-auto min-h-screen space-y-10 px-6 pb-16 pt-20 md:space-y-14 md:pt-32 lg:px-8 2xl:max-w-7xl">
       {children}
     </main>
   </>
