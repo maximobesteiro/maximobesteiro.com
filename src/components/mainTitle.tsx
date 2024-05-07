fb700319513d07718cef4ea2b0aa8857842b1e43
Máximo Besteiro <machibest@gmail.com>
2024-05-07 02:05:30 +0200

    Improved Brand experience for responsive

--- src/components/mainTitle.tsx
+++ src/components/mainTitle.tsx
@@ -9,24 +9,8 @@ interface MainTitleProps {
   wrapperRef: RefObject<HTMLElement>;
 }
 
-const viewBoxSizes = {
-  mobile: {
-    width: 92,
-    height: 35,
-  },
-  tablet: {
-    width: 192,
-    height: 27,
-  },
-  desktop: {
-    width: 192,
-    height: 27,
-  },
-};
-
 const MainTitle = ({ cursor, wrapperRef }: MainTitleProps) => {
   const deviceSize = useDeviceSize();
-  const [viewBox, setViewBox] = useState(viewBoxSizes[deviceSize]);
   const [gradientCenter, setGradientCenter] = useState({
     cx: '50%',
     cy: '50%',
@@ -36,7 +20,7 @@ const MainTitle = ({ cursor, wrapperRef }: MainTitleProps) => {
     if (wrapperRef.current && cursor.x !== null && cursor.y !== null) {
       const wrapperRect = wrapperRef.current.getBoundingClientRect();
       const cx = (cursor.x / wrapperRect.width) * 100;
-      const cy = (cursor.y / wrapperRect.height) * 100 + 20;
+      const cy = (cursor.y / wrapperRect.height) * 100;
       setGradientCenter({ cx: `${cx}%`, cy: `${cy}%` });
     }
   }, [cursor, wrapperRef]);
@@ -45,50 +29,71 @@ const MainTitle = ({ cursor, wrapperRef }: MainTitleProps) => {
     if (deviceSize !== 'desktop') {
       console.log('Animating gradient...');
     }
-    setViewBox(viewBoxSizes[deviceSize]);
   }, [deviceSize]);
 
   return (
-    <svg
-      viewBox={`0 0 ${viewBox.width} ${viewBox.height}`}
-      preserveAspectRatio="xMinYMin meet"
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-max w-max stroke-zinc-300 pb-8 lg:w-2/3"
-    >
-      <defs>
-        <radialGradient
-          id="emeraldGradientr"
-          gradientUnits="userSpaceOnUse"
-          r="10%"
-          cx={gradientCenter.cx}
-          cy={gradientCenter.cy}
+    <>
+      <svg
+        viewBox="0 0 192 27"
+        preserveAspectRatio="xMinYMin meet"
+        xmlns="http://www.w3.org/2000/svg"
+        className="hidden h-max  w-max stroke-zinc-300 pb-8 md:block lg:w-2/3"
+      >
+        <defs>
+          <radialGradient
+            id="emeraldGradientr"
+            gradientUnits="userSpaceOnUse"
+            r="12%"
+            cx={gradientCenter.cx}
+            cy={gradientCenter.cy}
+          >
+            {true && <stop offset="0%" stopColor="#dbaa39" />}
+            <stop offset="100%" stopColor="#4a463b" />
+          </radialGradient>
+        </defs>
+        <text
+          className="fill-zinc-100 stroke-[0.3] font-sans text-2xl font-black"
+          style={{ fillOpacity: 0.03 }}
+          x="0"
+          y="95%"
+          stroke="url(#emeraldGradientr)"
         >
-          {true && <stop offset="0%" stopColor="#dbaa39" />}
-          <stop offset="100%" stopColor="#4a463b" />
-        </radialGradient>
-      </defs>
-      <text
-        className="hidden fill-zinc-100 stroke-[0.3] font-sans text-2xl font-black md:block"
-        style={{ fillOpacity: 0.03 }}
-        x="0"
-        y="95%"
-        stroke="url(#emeraldGradientr)"
+          Máximo Besteiro
+        </text>
+      </svg>
+
+      <svg
+        viewBox="0 0 72 35"
+        preserveAspectRatio="xMinYMin meet"
+        xmlns="http://www.w3.org/2000/svg"
+        className="block h-max w-2/3 stroke-zinc-300 py-4 md:hidden lg:w-2/3"
       >
-        Máximo Besteiro
-      </text>
-      <text
-        className="block fill-zinc-100 stroke-[0.3] font-sans text-lg font-black md:hidden"
-        style={{ fillOpacity: 0.03 }}
-        stroke="url(#emeraldGradientr)"
-      >
-        <tspan x="0" y="95%">
-          Máximo
-        </tspan>
-        <tspan x="0" y="200%">
-          Besteiro
-        </tspan>
-      </text>
-    </svg>
+        <defs>
+          <radialGradient
+            id="emeraldGradientr2"
+            gradientUnits="userSpaceOnUse"
+            r="25%"
+            cx={gradientCenter.cx}
+            cy={gradientCenter.cy}
+          >
+            {true && <stop offset="0%" stopColor="#dbaa39" />}
+            <stop offset="100%" stopColor="#4a463b" />
+          </radialGradient>
+        </defs>
+        <text
+          className="block fill-zinc-100 stroke-[0.3] font-sans text-lg font-black md:hidden"
+          style={{ fillOpacity: 0.03 }}
+          stroke="url(#emeraldGradientr2)"
+        >
+          <tspan x="2" y="15">
+            Máximo
+          </tspan>
+          <tspan x="0" y="32">
+            Besteiro
+          </tspan>
+        </text>
+      </svg>
+    </>
   );
 };
 
